50d699f544b80a43e456e261c7563ada179c7ca8
stan <stanislav.benet@gmail.com>
2024-08-13 20:41:36 +0100

    fixed type issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"React-vite-app",
-    short_name:"react-vite-app",
-    description:"I am a simple vite app",
-    icons:[{
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Benfit",
+    short_name: "Benfit",
+    description: "I am a simple fitness app",
+    icons: [{
       src: '/android-chrome-192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'favicon'
     },
     {
-      src:'/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'favicon'
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
     },
     {
       src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
     },
     {
       src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
     }
-  ],
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
   }
 }
 
